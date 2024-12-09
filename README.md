f9d74ce7c975b9c9686ac22693fdf777171a402b
engeir <engeir@pm.me>
2024-12-09 15:22:24 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# NcDump Rich previewer
+
+## Install
+
+```bash
+ya pack -a engeir/ncdump-rich.yazi
+```
+
+## Dependencies
+
+- ncdump-rich
+
+  ```bash
+  uv tool install ncdump-rich
+  # uvx ncdump-rich
+  ```
---
+++ init.lua
@@ -0,0 +1,11 @@
+-- NOTE: see https://yazi-rs.github.io/docs/plugins/overview/#previewer as well as
+-- https://github.com/yazi-rs/plugins for inspiration.
+M = {}
+return {
+    peek = function(self)
+        return
+    end,
+    seek = function(self)
+        return
+    end,
+}
